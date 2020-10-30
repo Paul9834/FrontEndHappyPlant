3d9e4d0c7363fe6f23d13d0d68a712d9bb78975d
Kurosou <43392610+Kurosou@users.noreply.github.com>
2020-10-30 10:16:38 -0500

    Servicio planta (con search by id)
    Se añade el servicio `para las plantas

---
+++ src/app/service/plant-info.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Planta } from '../models/plant.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserInfoService {
+
+  _urlPlantas: string;
+
+
+  constructor(private http: HttpClient) {
+    this._urlPlantas = "https://happyplantbackend.herokuapp.com/planta";
+   }
+  createUser(newPlant: Planta) {
+        this.http.post(this._urlPlantas, newPlant).subscribe(plant => console.log(plant));
+  }
+
+  getPlantById(id: string): Observable <any> {
+
+    let url = this._urlPlantas +'/log/' + id;
+
+    let headers = new HttpHeaders().set(
+      'Type-Content','aplication/json'
+    )
+
+    return this.http.get(url, {
+      headers: headers
+    });
+
+  }
+
+
+
+
+}
